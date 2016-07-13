42c7135949245e7e6842a97ae121400c75590ccf
schaganti <sivatej.vtz059@gmail.com>
2016-07-13 05:58:06 +0000

    updated deploy.rb with rbenv_type

--- config/deploy.rb
+++ config/deploy.rb
@@ -30,7 +30,7 @@ set :repo_url, 'git@github.com:shiva-chaganti/test_app.git'
  set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system')
 
 
-set :rbenv_type, :system
+set :rbenv_type, :user
 set :rbenv_ruby, '2.3.0'
 #set :rbenv_custom_path, '/root/.rbenv/bin/'
 set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
